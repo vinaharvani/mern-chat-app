8ab5c6041beb7755267fe6f0ed1b288dc2c0ff9a
vinaharvani <vinaharvani2000@gmail.com>
2025-12-20 02:39:24 +0530

    allow the frontend url to access backend

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use("/api/messages", messageRouter);
 // Socket.IO
 export const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://mern-chat-app-frontend-ruddy.vercel.app",
         credentials: true
     }
 });
